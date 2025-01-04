0037ca6c393d7dcd1f8f7838e0a06cec4e75b0a0
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2025-01-04 20:59:15 +0200

    Create home page

--- food_app/lib/main.dart
+++ food_app/lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:food_app/auth/login_or_register.dart';
-import 'package:food_app/pages/login_page.dart';
-import 'package:food_app/pages/register_page.dart';
 import 'package:food_app/themes/theme_provider.dart';
 import 'package:provider/provider.dart';
 
--- food_app/lib/pages/login_page.dart
+++ food_app/lib/pages/login_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:food_app/components/my_button.dart';
 import 'package:food_app/components/my_textfield.dart';
+import 'package:food_app/pages/home_page.dart';
 
 class LoginPage extends StatefulWidget{
   final void Function()? onTap;
@@ -14,6 +15,15 @@ class LoginPage extends StatefulWidget{
 class _LoginPageState extends State<LoginPage> {
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
+
+  void login(){
+
+    Navigator.push(
+      context, 
+      MaterialPageRoute(builder: (context) => const HomePage()
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -60,7 +70,7 @@ class _LoginPageState extends State<LoginPage> {
 
             MyButton(
               text: "Sign In", 
-              onTap: () {},
+              onTap: login,
             ),
 
             const SizedBox(height: 25),
