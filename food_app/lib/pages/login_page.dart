ad74882d0d4efaa45d186d3cc78f4268694f5201
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2025-01-04 17:04:59 +0200

    Create project and theme classes

---
+++ food_app/lib/pages/login_page.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:food_app/components/my_textfield.dart';
+
+class LoginPage extends StatelessWidget{
+
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  LoginPage( {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.lock_open_rounded,
+              size: 72,
+              color: Theme.of(context).colorScheme.inversePrimary,
+            ),
+
+            const SizedBox(height:25),
+
+            Text(
+              "Food Delivery App",
+              style: TextStyle(
+                fontSize: 16,
+                color: Theme.of(context).colorScheme.inversePrimary,
+              ),
+            ),
+
+            const SizedBox(height:25),
+
+            MyTextField(
+              controller: emailController, 
+              hintText: "Email", 
+              obscureText: false,
+            ),
+
+          ],
+        ),
+      ),
+    );
+  }
+}
