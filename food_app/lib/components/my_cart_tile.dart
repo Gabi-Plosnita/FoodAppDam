88d56ac3e72a13cf3a7b5996f6c332dfce2b9810
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2025-01-06 17:57:01 +0200

    Update my_cart_tile.dart

--- food_app/lib/components/my_cart_tile.dart
+++ food_app/lib/components/my_cart_tile.dart
@@ -22,6 +22,7 @@ class MyCartTile extends StatelessWidget {
             Padding(
               padding: const EdgeInsets.all(8.0),
               child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   ClipRRect(
                     borderRadius: BorderRadius.circular(8),
@@ -36,7 +37,12 @@ class MyCartTile extends StatelessWidget {
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       Text(cartItem.food.name),
-                      Text("\$${cartItem.food.price}"),
+                      Text(
+                        "\$${cartItem.food.price}",
+                        style: TextStyle(
+                          color: Theme.of(context).colorScheme.primary,
+                        ),
+                      ),
                     ],
                   ),
                   const Spacer(),
@@ -54,6 +60,40 @@ class MyCartTile extends StatelessWidget {
                 ],
               ),
             ),
+            SizedBox(
+              height: cartItem.selectedAddons.isEmpty ? 0 : 60,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                padding: EdgeInsets.only(left: 10, bottom: 10, right: 10),
+                children: cartItem.selectedAddons
+                    .map(
+                      (addon) => Padding(
+                        padding: const EdgeInsets.only(right: 8.0),
+                        child: FilterChip(
+                          label: Row(
+                            children: [
+                              Text(addon.name),
+                              Text(" (+\$${addon.price})"),
+                            ],
+                          ),
+                          shape: StadiumBorder(
+                            side: BorderSide(
+                              color: Theme.of(context).colorScheme.primary,
+                            ),
+                          ),
+                          onSelected: (value) {},
+                          backgroundColor:
+                              Theme.of(context).colorScheme.secondary,
+                          labelStyle: TextStyle(
+                            color: Theme.of(context).colorScheme.inversePrimary,
+                            fontSize: 12,
+                          ),
+                        ),
+                      ),
+                    )
+                    .toList(),
+              ),
+            )
           ],
         ),
       ),
