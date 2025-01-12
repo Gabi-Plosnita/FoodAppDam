07cbe61a3421ffae1eb337a868d902dd39ad4c9b
Gabi-Plosnita <118562250+Gabi-Plosnita@users.noreply.github.com>
2025-01-12 19:51:25 +0200

    Load the menu from firestore instead of local  list

--- food_app/lib/models/food.dart
+++ food_app/lib/models/food.dart
@@ -14,6 +14,24 @@ class Food {
     required this.category,
     required this.availableAddons,
   });
+
+  Map<String, dynamic> toJson() => {
+    'name': name,
+    'description': description,
+    'imagePath': imagePath,
+    'price': price,
+    'category': category.toString().split('.').last,
+    'availableAddons': availableAddons.map((addon) => addon.toJson()).toList(),
+  };
+
+  factory Food.fromJson(Map<String, dynamic> json) => Food(
+    name: json['name'],
+    description: json['description'],
+    imagePath: json['imagePath'],
+    price: json['price'],
+    category: FoodCategory.values.firstWhere((e) => e.toString().split('.').last == json['category']),
+    availableAddons: List<Addon>.from(json['availableAddons'].map((addon) => Addon.fromJson(addon))),
+  );
 }
 
 enum FoodCategory {
@@ -30,4 +48,14 @@ class Addon{
     required this.name,
     required this.price
   });
+
+   Map<String, dynamic> toJson() => {
+    'name': name,
+    'price': price,
+  };
+
+  factory Addon.fromJson(Map<String, dynamic> json) => Addon(
+    name: json['name'],
+    price: json['price'],
+  );
 }
